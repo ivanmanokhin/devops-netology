5d7354603678707e82fd3591d0a21b2bdb2493f6
ivanmanokhin <manokhin@zoho.com>
2022-09-10 14:27:35 +0300

    add homework

---
+++ homeworks/06. Виртуализация, базы данных и Terraform/04. Оркестрация группой Docker контейнеров на примере Docker Compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gh5p5qgslmcei5io0i"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g51tksi3aq7j9inm1k"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8559184hi47039oadg"
+}
